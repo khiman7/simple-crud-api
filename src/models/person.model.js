7ea0d945a1f4a987cf673bbe332c94fd2b967b31
Kyrylo Khiman <kyrylkhiman@gmail.com>
2021-11-28 01:26:47 +0200

    feat: add person model

---
+++ src/models/person.model.js
@@ -0,0 +1,42 @@
+const { v4: uuidv4 } = require('uuid');
+
+const persons = require('../db/persons.json');
+
+class Person {
+  async findAll() {
+    return persons;
+  }
+
+  async findById(id) {
+    const person = persons.find(person => person.id === id);
+
+    return person;
+  }
+
+  async create(person) {
+    const newPerson = { id: uuidv4(), ...person};
+
+    persons.push(newPerson);
+
+    return newPerson;
+  }
+
+  async update(id, personData) {
+    const person = persons.find(person => person.id === id);
+
+    person = {...person, ...personData};
+
+    return person;
+  }
+
+  async delete(id) {
+    const index = persons.findIndex(person => person.id === id);
+    const deletedPerson = persons[index];
+
+    persons.splice(index, 1);
+
+    return deletedPerson;
+  }
+}
+
+module.exports = new Person();
