9c219277510786f23d988608d9969faf7fa1eb97
Kyrylo Khiman <kyrylkhiman@gmail.com>
2021-11-28 19:07:50 +0200

    feat: minor changes

--- src/routes/person.routes.js
+++ src/routes/person.routes.js
@@ -4,8 +4,9 @@ const PersonController = require('../controllers/person.controller');
 const router = new Router();
 
 router.get('/person', PersonController.getPersons);
+router.get('/person/:id', PersonController.getPerson);
 router.post('/person', PersonController.createPerson);
-router.put('/person', () => { console.log('update some person'); });
-router.delete('/person', () => { console.log('delete some person'); });
+router.put('/person/:id', PersonController.updatePerson);
+router.delete('/person/:id', PersonController.deletePerson);
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const router = require('./utils/Router');
+const router = require('./routes/person.routes');
 
 const server = http.createServer((req, res) => {
   router.handle(req, res);
